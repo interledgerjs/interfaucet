dd9f517984595aa8d46cf81f804f9e81495ee5f3
Michiel de Jong <michiel@ripple.com>
2017-11-20 12:48:31 +0100

    feat: support LetsEncrypt and post-IPR flow

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const http = require('http')
+const startServer = require('./startServer')
 const Plugin = require('ilp-plugin-btp-client')
 const IlpPacket = require('ilp-packet')
 const uuid = require('uuid/v4')
+const url = require('url')
 function base64url (buf) { return buf.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '') }
 
 const plugin = new Plugin({
-  btpUri: 'btp+wss://interfaucet:' + process.env.TOKEN + '@amundsen.michielbdejong.com/api/17q3',
+  btpUri: 'btp+wss://interfaucet:' + process.env.TOKEN + '@amundsen.ilpdemo.org/api/17q3',
 })
 
-function getQuote(ippBuf) {
-  const ipp = IlpPacket.deserializeIlpPayment(ippBuf)
+function getQuote(ipp) {
   console.log('ipp', JSON.stringify(ipp))
   const quotePacket = IlpPacket.serializeIlqpByDestinationRequest({
     destinationAccount: ipp.account,
@@ -48,14 +48,43 @@ function sendTransfer(sourceAmount, ipp, condition) {
   return plugin.sendTransfer(transfer)
 }
 
+function pay(ipr, res) {
+  console.log('paying', ipr)
+  const ipp = IlpPacket.deserializeIlpPayment(ipr.packet)
+  return getQuote(ipp).then(quoteResponse => {
+    if (quoteResponse.sourceHoldDuration > 3600000) {
+      return Promise.reject(new Error('That would cost the Interfaucet more than 1 hour!'))
+    }
+    if (parseInt(quoteResponse.sourceAmount) > 1000) {
+      return Promise.reject(new Error('That would cost the Interfaucet more than 1000 microdollars!'))
+    }
+    return sendTransfer(quoteResponse.sourceAmount, ipr.packet, ipr.condition).then(() => {
+      res.end(`<html><h2>Congrats!</h2><p>Sent ${quoteResponse.sourceAmount} microdollars</p><img src="https://i.pinimg.com/564x/88/84/85/888485cae122717788328b4486803a32.jpg"></html>`)
+    })
+  })
+}
 
+console.log('connecting plugin, will start webserver soon!')
 plugin.connect().then(() => {
   console.log('client started, starting webserver')
-  const server = http.createServer((req, res) => {
+  startServer((req, res) => {
     Promise.resolve().then(() => {
       const parts = req.url.split('/')
       if (parts.length < 3) {
-        res.end('<html><h2>Welcome to Interfaucet!</h2><p>See <a href="https://michielbdejong.github.io/tutorials/payment-requests">the payment requests tutorial</a>.</p>')
+        const queryData = url.parse(req.url, true).query
+        console.log(queryData)
+        if (queryData.address && queryData.condition) {
+          return pay({
+            packet: IlpPacket.serializeIlpPayment({
+              account: queryData.address,
+              amount: '1000',
+              data: new Buffer([])
+            }),
+            condition: base64url(Buffer.from(queryData.condition, 'hex'))
+          }, res)
+        }
+        console.log (req.url)
+        res.end('<html><h2>Welcome to Interfaucet!</h2><p>See <a href="https://github.com/michielbdejong/ilp-plugin-stripe/pull/2">the payment requests tutorial</a>.</p>')
         return
       }
       console.log('interfaucet request!', parts)
@@ -66,21 +95,10 @@ plugin.connect().then(() => {
         condition: iprBuf.slice(-32)
       }
       console.log('ipr', JSON.stringify(ipr))
-      return getQuote(ipr.packet).then(quoteResponse => {
-        if (quoteResponse.sourceHoldDuration > 3600000) {
-          return Promise.reject(new Error('That would cost the Interfaucet more than 1 hour!'))
-        }
-        if (parseInt(quoteResponse.sourceAmount) > 1000) {
-          return Promise.reject(new Error('That would cost the Interfaucet more than 1000 microdollars!'))
-        }
-        return sendTransfer(quoteResponse.sourceAmount, ipr.packet, ipr.condition).then(() => {
-          res.end(`<html><h2>Congrats!</h2><p>Sent ${quoteResponse.sourceAmount} microdollars</p><img src="https://i.pinimg.com/564x/88/84/85/888485cae122717788328b4486803a32.jpg"></html>`)
-        })
-      })
+      return pay(ipr, res)
     }).catch(err => {
       console.log(err, err.message)
       res.end('<html><h2>Oops! Something went wrong.</h2><p>' + err.message + '</p><img src="https://i.pinimg.com/736x/fa/d2/76/fad27608b9bd588fe18231e2babe2b5f--man-faces-strange-places.jpg"></html>')
     })
   })
-  server.listen(process.env.PORT)
 })
